771683152e229df8c3efcd3ed0c67c4323449114
michalm57 <mroczekmichal57@gmail.com>
2023-06-13 19:41:04 +0200

    create employees table

---
+++ Modules/Employees/Database/Migrations/2023_06_13_173750_create_employees_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('first_name')->nullable();
+            $table->text('last_name')->nullable();
+            $table->unsignedBigInteger('department_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('department_id')->references('id')->on('departments');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('employees');
+    }
+};
