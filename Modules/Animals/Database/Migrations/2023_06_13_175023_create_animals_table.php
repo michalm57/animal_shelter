8937717432c69f113b533fb84ed751d9fdffba95
michalm57 <mroczekmichal57@gmail.com>
2023-06-13 20:02:21 +0200

    create animal table

---
+++ Modules/Animals/Database/Migrations/2023_06_13_175023_create_animals_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+use Modules\Tasks\Enums\AnimalTypeEnum;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('animals', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name')->nullable();
+            $table->enum('name', AnimalTypeEnum::getEnumValues());
+            $table->unsignedBigInteger('employee_id')->nullable();
+            $table->timestamps();
+
+            $table->foreign('employee_id')->references('id')->on('employees');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('animals');
+    }
+};
