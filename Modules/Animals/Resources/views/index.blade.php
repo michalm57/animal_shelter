6f207879cac80d8be4f0b064b90912e73a7753bd
michalm57 <mroczekmichal57@gmail.com>
2023-06-13 19:30:40 +0200

    create modules

---
+++ Modules/Animals/Resources/views/index.blade.php
@@ -0,0 +1,9 @@
+@extends('animals::layouts.master')
+
+@section('content')
+    <h1>Hello World</h1>
+
+    <p>
+        This view is loaded from module: {!! config('animals.name') !!}
+    </p>
+@endsection
---
+++ Modules/Departments/Resources/views/layouts/master.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Module Departments</title>
+
+       {{-- Laravel Vite - CSS File --}}
+       {{-- {{ module_vite('build-departments', 'Resources/assets/sass/app.scss') }} --}}
+
+    </head>
+    <body>
+        @yield('content')
+
+        {{-- Laravel Vite - JS File --}}
+        {{-- {{ module_vite('build-departments', 'Resources/assets/js/app.js') }} --}}
+    </body>
+</html>
