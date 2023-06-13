4b64b1d7d6c01c3719d01618af00cc77c8d11f58
michalm57 <mroczekmichal57@gmail.com>
2023-06-13 20:22:39 +0200

    create animal controller

--- Modules/Animals/Http/Controllers/AnimalsController.php
+++ Modules/Animals/Http/Controllers/AnimalsController.php
@@ -2,78 +2,112 @@
 
 namespace Modules\Animals\Http\Controllers;
 
-use Illuminate\Contracts\Support\Renderable;
-use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
+use Modules\Animals\Resources\AnimalResource;
+use Modules\Animals\Services\AnimalService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Modules\Animals\Http\Requests\AnimalRequest;
 
 class AnimalsController extends Controller
 {
+    private $animalService;
+
     /**
-     * Display a listing of the resource.
-     * @return Renderable
+     * AnimalsController constructor.
+     * 
+     * @param AnimalService $animalService The animal service instance.
+     */
+    public function __construct(AnimalService $animalService)
+    {
+        $this->animalService = $animalService;
+    }
+
+    /**
+     * Display a listing of animals.
+     * 
+     * @return JsonResponse
      */
     public function index()
     {
-        return view('animals::index');
+        return response()->json([
+            'data' => AnimalResource::collection($this->animalService->getAll()),
+        ]);
     }
 
     /**
-     * Show the form for creating a new resource.
-     * @return Renderable
+     * Store a newly created animal in storage.
+     * 
+     * @param AnimalRequest $request The HTTP request object.
+     * @return JsonResponse
      */
-    public function create()
+    public function store(AnimalRequest $request)
     {
-        return view('animals::create');
+        try {
+            $this->animalService->create($request->validated());
+
+            return new JsonResponse(
+                ['status' => 'success', 'message' => 'Animal has been added successfully!'],
+                Response::HTTP_OK
+            );
+        } catch (\Exception $exception) {
+            report($exception);
+
+            return new JsonResponse(
+                ['status' => 'error', 'message' => 'Unable to add animal!'],
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
     }
 
     /**
-     * Store a newly created resource in storage.
-     * @param Request $request
-     * @return Renderable
+     * Update the specified animal in storage.
+     *
+     * @param AnimalRequest $request The HTTP request object.
+     * @param int $id The ID of the animal.
+     * @return JsonResponse
      */
-    public function store(Request $request)
+    public function update(AnimalRequest $request, $id)
     {
-        //
+        try {
+            $this->animalService->update($id, $request->validated());
+
+            return new JsonResponse(
+                ['status' => 'success', 'message' => 'Animal has been updated successfully!'],
+                Response::HTTP_OK
+            );
+        } catch (\Exception $exception) {
+            report($exception);
+
+            return new JsonResponse(
+                ['status' => 'error', 'message' => 'Unable to update animal!'],
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
     }
 
     /**
-     * Show the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function show($id)
-    {
-        return view('animals::show');
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     * @param int $id
-     * @return Renderable
-     */
-    public function edit($id)
-    {
-        return view('animals::edit');
-    }
-
-    /**
-     * Update the specified resource in storage.
-     * @param Request $request
-     * @param int $id
-     * @return Renderable
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     * @param int $id
-     * @return Renderable
+     * Remove the specified animal from storage.
+     *
+     * @param int $id The ID of the animal.
+     * @return JsonResponse
      */
     public function destroy($id)
     {
-        //
+        try {
+            $this->animalService->delete($id);
+
+            return new JsonResponse(
+                ['status' => 'success', 'message' => 'Animal has been deleted successfully!'],
+                Response::HTTP_OK
+            );
+        } catch (\Exception $exception) {
+            report($exception);
+
+            return new JsonResponse(
+                ['status' => 'error', 'message' => 'Unable to delete animal!'],
+                Response::HTTP_UNPROCESSABLE_ENTITY
+            );
+        }
     }
 }
